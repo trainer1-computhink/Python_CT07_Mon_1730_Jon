59c9c121446569f288d15ab507b32c83427b0a67
trainer1-computhink <trainer1@computhink.com.sg>
2026-01-12 11:00:04 +0000

    lesson 2

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,30 @@
-print("Hello from lesson 1")
+# print("Hello from lesson 1")
 
+# Robot will check the material
+# if the material is glass, put into the bin for glass
+# else if the material is plastics, put into the bin for plastics
+# else put into the bin for paper
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+
+# for i in range(0,101):
+#     print(i)
+
+# for char in "lucius":
+#     print(char) 
+
+# for i in range(100, 10, -10):
+#     print(i)
+
+for i in range(50,100,5):
+    print(i)
